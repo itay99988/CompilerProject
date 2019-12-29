6fb44436b224c7cf036385c0a70f907a898afb05
Itay Cohen <itay99988@gmail.com>
2019-12-29 14:09:31 +0200

    done with SemantMe() for AST_ARR_DEC

--- EX3/FOLDER_2_SRC/AST/AST_ARR_DEC.java
+++ EX3/FOLDER_2_SRC/AST/AST_ARR_DEC.java
@@ -32,7 +32,31 @@ public class AST_ARR_DEC extends AST_DEC {
 
 	public TYPE SemantMe() throws SemantException 
 	{
-		return null;
+		String err;
+
+        TYPE t = SYMBOL_TABLE.getInstance().find(this.type, EntryCategory.Type);
+        boolean isArrayDecNameExist = SYMBOL_TABLE.getInstance().existInScope(this.name);
+
+        if (t == null) 
+		{ //array element type does not exist
+            err = String.format(">> ERROR array_dec: %s %s, type doesn't exist\n", this.name, this.type);
+            throw new SemantException(this.getLineNumber(), err);
+        }
+        if (isArrayDecNameExist) 
+		{
+            err = String.format(">> ERROR array declaration name %s already exist in the current scope level", this.name);
+            throw new SemantException(this.getLineNumber(), err);
+        } 
+        if(t == TYPE_VOID.getInstance())
+		{
+        	err = String.format(">> ERROR array declaration name %s, can't use type void\n", this.name);
+            throw new SemantException(this.getLineNumber(), err);
+        }
+        else 
+		{
+            SYMBOL_TABLE.getInstance().enter(this.name, new TYPE_ARRAY(this.type, 0), EntryCategory.Type);
+        }
+        return null;
 	}
 
 }
