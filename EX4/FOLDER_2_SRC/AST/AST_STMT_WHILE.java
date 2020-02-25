6161b2881ae1c803fa5072a0bb168842c90c9818
Itay Cohen <itay99988@gmail.com>
2020-02-25 16:58:57 +0200

    while statement mipsme() addition

--- EX4/FOLDER_2_SRC/AST/AST_STMT_WHILE.java
+++ EX4/FOLDER_2_SRC/AST/AST_STMT_WHILE.java
@@ -3,7 +3,7 @@ package AST;
 import TYPES.*;
 import SYMBOL_TABLE.*;
 import TEMP.*;
-import IR.*;
+import MIPS.*;
 
 public class AST_STMT_WHILE extends AST_STMT
 {
@@ -80,56 +80,18 @@ public class AST_STMT_WHILE extends AST_STMT
 		return null;
 	}
 	
-	public TEMP IRme()
+	public void MIPSme()
 	{
-		/*******************************/
-		/* [1] Allocate 2 fresh labels */
-		/*******************************/
-		String label_end   = IRcommand.getFreshLabel("end");
-		String label_start = IRcommand.getFreshLabel("start");
-	
-		/*********************************/
-		/* [2] entry label for the while */
-		/*********************************/
-		IR.
-		getInstance().
-		Add_IRcommand(new IRcommand_Label(label_start));
-
-		/********************/
-		/* [3] cond.IRme(); */
-		/********************/
-		TEMP cond_temp = cond.IRme();
-
-		/******************************************/
-		/* [4] Jump conditionally to the loop end */
-		/******************************************/
-		IR.
-		getInstance().
-		Add_IRcommand(new IRcommand_Jump_If_Eq_To_Zero(cond_temp,label_end));		
-
-		/*******************/
-		/* [5] body.IRme() */
-		/*******************/
-		body.IRme();
-
-		/******************************/
-		/* [6] Jump to the loop entry */
-		/******************************/
-		IR.
-		getInstance().
-		Add_IRcommand(new IRcommand_Jump_Label(label_start));		
-
-		/**********************/
-		/* [7] Loop end label */
-		/**********************/
-		IR.
-		getInstance().
-		Add_IRcommand(new IRcommand_Label(label_end));
-
-		/*******************/
-		/* [8] return null */
-		/*******************/
-		return null;
+		sir_MIPS_a_lot mips = sir_MIPS_a_lot.getInstance();
+		String while_start = sir_MIPS_a_lot.getFreshLabel("while_start");
+		String while_end = sir_MIPS_a_lot.getFreshLabel("while_end");
+		
+		mips.label(while_start); //loop entrance
+    	TEMP condition_eval = cond.MIPSme(); //evaluate condition
+    	mips.beqz(condition_eval, while_end);
+    	body.MIPSme(); //loop body
+    	mips.jump(while_start);
+    	mips.label(while_end);
 	}
 	
 }
